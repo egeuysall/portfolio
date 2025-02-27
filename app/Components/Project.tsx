d237d787634d82cb2faf01dd0f1cda9ea6b1c9a6
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-27 16:15:26 -0600

    Debug

--- app/Components/Project.tsx
+++ app/Components/Project.tsx
@@ -14,7 +14,7 @@ type ProjectProps = {
 const Project = ({ img, name, desc, tags }: ProjectProps) => {
   return (
     <Link href="/">
-      <div className="p-8 border-2 border-[#cccccc] rounded-md bg-[#ffffff] dark:bg-[#595959] dark:border-[#7f7f7f] box-border h-120">
+      <div className="p-8 border-2 border-[#cccccc] rounded-md bg-[#ffffff] dark:bg-[#595959] dark:border-[#7f7f7f] box-border min-h-[30rem] md:min-h-[35rem] lg:min-h-[40rem] flex flex-col justify-between">
         <div className="flex justify-center h-48 overflow-hidden rounded-md">
           <Image
             src={img}
@@ -27,10 +27,10 @@ const Project = ({ img, name, desc, tags }: ProjectProps) => {
         <h2 className={`${lato.className} text-3xl font-bold text-[#2e2e2e] dark:text-[#f2f2f2] mt-3`}>
           {name}
         </h2>
-        <p className="text-[#7f7f7f] dark:text-[#cccccc] text-xl font-normal mt-2">
+        <p className="text-[#7f7f7f] dark:text-[#cccccc] text-xl font-normal mt-2 flex-grow">
           {desc}
         </p>
-        <div className="flex gap-2 mt-4">
+        <div className="flex gap-2 mt-4 justify-end flex-wrap">
           {tags.map((tag, index) => (
             <span
               key={index}

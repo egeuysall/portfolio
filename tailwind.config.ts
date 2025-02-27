e2f8d925bfdca9ebbcab12fe776978325ed71dfa
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-27 10:30:57 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,12 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'media', // Ensure dark mode is based on the user's system preference
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)",  // Use CSS variable for background color
+        foreground: "var(--foreground)",  // Use CSS variable for text color
       },
     },
   },

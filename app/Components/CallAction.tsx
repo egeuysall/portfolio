e7ea1ebad6312c6faca66abd1dc9bceb0bc95716
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-28 17:04:50 -0600

    Debug

--- app/Components/CallAction.tsx
+++ app/Components/CallAction.tsx
@@ -11,7 +11,7 @@ interface CallActionProps {
 const CallAction = ({ name, link }: CallActionProps) => {
     return (
         <Link href={link} aria-label="Call-to-action button">
-            <button className={`${lato.className} className="mb-3 mr-3" bg-[#7f8c8f] w-full md:w-1/2 py-3 rounded-md cursor-pointer font-bold text-[#ecf0f1] hover:scale-105 hover:opacity-80 active:opacity-70 transition duration-300`}>
+            <button className={`${lato.className} bg-[#7f8c8f] w-full md:w-auto py-3 px-6 rounded-md cursor-pointer font-bold text-[#ecf0f1] hover:scale-105 hover:opacity-80 active:opacity-70 transition duration-300 mb-3 mr-3`}>
                 {name}
             </button>
         </Link>

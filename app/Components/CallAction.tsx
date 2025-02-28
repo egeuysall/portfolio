069dc04e96cdda96b87b8b5e2c063d6e8e303691
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-28 15:21:53 -0600

    Debug

--- app/Components/CallAction.tsx
+++ app/Components/CallAction.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const CallAction = ( name ) => {
+const CallAction = ( name:String ) => {
     return (
         <Link href="/" aria-label="Call-to-action button">
             <button className={`${lato.className} bg-[#7f8c8f] w-full md:w-1/2 py-3 rounded-md cursor-pointer font-bold text-[#ecf0f1] hover:scale-105 hover:opacity-80 active:opacity-70 transition duration-300`}>

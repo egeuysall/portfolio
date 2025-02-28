839840e2bc3db3a89bdcd6238d5dc2a83a63bbb2
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-28 15:21:53 -0600

    Debug

--- app/Components/ThemeToggleButton.tsx
+++ app/Components/ThemeToggleButton.tsx
@@ -34,9 +34,9 @@ const ThemeToggleButton = () => {
       className="fixed bottom-4 left-4 p-3 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 shadow-lg"
     >
       {theme === 'light' ? (
-        <img src="/assets/custom-moon-icon.svg" alt="Moon icon" width={20} height={20} />
+        <img src="/assets/moon.svg" alt="Moon icon" width={20} height={20} />
       ) : (
-        <img src="/assets/custom-sun-icon.svg" alt="Sun icon" width={20} height={20} />
+        <img src="/assets/sun.svg" alt="Sun icon" width={20} height={20} />
       )}
     </button>
   );

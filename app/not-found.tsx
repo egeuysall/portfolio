6ae6ded44958fca7046d2e7ecb566504b2954068
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-28 22:29:24 -0600

    Revert

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import CallAction from "./Components/CallAction";
 
-const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
+const lato = Lato({ subsets: ["latin"], weight: ["700"] });
 
 export default function NotFound() {
   const [message, setMessage] = useState("Page Lost in the Matrix");
@@ -20,41 +20,33 @@ export default function NotFound() {
 
   return (
     <div
-      className={`${lato.className} flex flex-col items-center justify-center h-screen w-screen text-center p-4 bg-[#f2f2f2] dark:bg-[#2e2e2e]`}
+      className={`${lato.className} flex flex-col items-center justify-center min-h-screen text-center p-4 bg-[#f2f2f2] dark:bg-[#2e2e2e] md:flex-row md:gap-24 lg:gap-32`}
     >
-      <div className="flex-shrink-0 mb-8">
+      <div>
         <Image
           src="/assets/404.svg"
           alt="404 Illustration"
           width={300}
           height={300}
-          className="w-full max-w-xs lg:max-w-sm dark:brightness-125 hover:grayscale-[50%] transition duration-300"
+          className="mb-8 dark:brightness-125 hover:grayscale-[50%] transition duration-300 w-[28rem]"
         />
       </div>
-      <div className="flex-grow text-center md:text-left">
+      <div>
         <h1
           className={`${lato.className} text-7xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] hidden md:block md:text-9xl`}
         >
           404
         </h1>
         <h2
-          className={`${lato.className} text-3xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] md:text-5xl`}
+          className={`${lato.className} text-3xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] md:text-5xl cursor-pointer`}
+          onClick={toggleMessage}
         >
-          Page Lost in the Matrix
+          {message}
         </h2>
-        <div className="mb-4">
-          <p
-            className={`${lato.className} text-xl text-[#2e2e2e] dark:text-[#f2f2f2] cursor-pointer font-normal`}
-            onClick={toggleMessage}
-          >
-            {message}
-          </p>
-        </div>
-        <div className="w-full max-w-xs mx-auto">
-          <div className="w-full flex justify-center">
-            <CallAction name="Go Home" link="/" />
-          </div>
-        </div>
+          <CallAction 
+            name="Go Home"
+            link="/"
+          />
       </div>
     </div>
   );

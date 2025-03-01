a24565b518ff2f7b3ba2f6f28d4e36affa108c10
Ege Uysal <ahmetegeuysal@gmail.com>
2025-03-01 00:49:59 -0600

    Debug

--- app/Components/LayoutWrapper.tsx
+++ app/Components/LayoutWrapper.tsx
@@ -1,21 +1,42 @@
 "use client";
+
 import JsonLd from "./JsonLd";
 
-type JsonLdData = {
+interface JsonLdData {
   "@context": string;
   "@type": string;
   name: string;
+  url: string;
+  sameAs: string[];
   image: string;
   description: string;
-};
+  jobTitle: string;
+  worksFor: {
+    "@type": string;
+    name: string;
+  };
+  address: {
+    "@type": string;
+    streetAddress: string;
+    addressLocality: string;
+    addressRegion: string;
+    postalCode: string;
+    addressCountry: string;
+  };
+  contactPoint: {
+    "@type": string;
+    telephone: string;
+    contactType: string;
+    areaServed: string;
+    availableLanguage: string[];
+  };
+  creator: {
+    "@type": string;
+    name: string;
+  };
+}
 
-export default function LayoutWrapper({
-  jsonLdData,
-  children,
-}: {
-  jsonLdData: JsonLdData;
-  children: React.ReactNode;
-}) {
+export default function LayoutWrapper({ jsonLdData, children }: { jsonLdData: JsonLdData; children: React.ReactNode }) {
   return (
     <>
       <JsonLd jsonLdData={jsonLdData} />

d596f45131aca8bde9a4537ad6d94a0b6a1ae44a
Ege Uysal <ahmetegeuysal@gmail.com>
2025-02-28 22:29:24 -0600

    Debug

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ import CallAction from "./Components/CallAction";
 const lato = Lato({ subsets: ["latin"], weight: ["700"] });
 
 export default function NotFound() {
-  const [message, setMessage] = useState("Page Lost in the Matrix");
+  const [message, setMessage] = useState<string>("Page Lost in the Matrix");
 
   const toggleMessage = () => {
     setMessage((prevMessage) =>
@@ -19,10 +19,10 @@ export default function NotFound() {
   };
 
   return (
-    <div
+    <main
       className={`${lato.className} flex flex-col items-center justify-center min-h-screen text-center p-4 bg-[#f2f2f2] dark:bg-[#2e2e2e] md:flex-row md:gap-24 lg:gap-32`}
     >
-      <div>
+      <section>
         <Image
           src="/assets/404.svg"
           alt="404 Illustration"
@@ -30,24 +30,25 @@ export default function NotFound() {
           height={300}
           className="mb-8 dark:brightness-125 hover:grayscale-[50%] transition duration-300 w-[28rem]"
         />
-      </div>
-      <div>
-        <h1
-          className={`${lato.className} text-7xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] hidden md:block md:text-9xl`}
-        >
-          404
-        </h1>
-        <h2
+      </section>
+      <section className="flex flex-col items-center">
+        <header>
+          <h1
+            className={`${lato.className} text-7xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] hidden md:block md:text-9xl`}
+          >
+            404
+          </h1>
+        </header>
+        <p
           className={`${lato.className} text-3xl font-bold mb-4 text-[#2e2e2e] dark:text-[#f2f2f2] md:text-5xl cursor-pointer`}
           onClick={toggleMessage}
         >
           {message}
-        </h2>
-          <CallAction 
-            name="Go Home"
-            link="/"
-          />
-      </div>
-    </div>
+        </p>
+        <footer>
+          <CallAction name="Go Home" link="/" />
+        </footer>
+      </section>
+    </main>
   );
 }

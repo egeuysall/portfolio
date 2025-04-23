a09ed849f6bda68416ddd2c5c389a1d419439a73
Ege Uysal <hello@egeuysal.com>
2025-04-22 21:25:25 -0500

    Debug

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const notFound = () => {
+export default function NotFound() {
   return (
-    <div>not-found</div>
-  )
+    <main className="w-full h-[calc(100vh-16rem)] flex flex-col items-center justify-center text-center gap-6">
+      <Image
+        width={64}
+        height={64}
+        alt="Page not found icon"
+        src="/logos/header-logo.svg"
+        className="dark:invert mx-auto"
+      />
+      <h2>Sorry, we couldn&apos;t find that page.</h2>
+      <Link href="/">
+        <button className="btn-2">Go home</button>
+      </Link>
+    </main>
+  );
 }
-
-export default notFound

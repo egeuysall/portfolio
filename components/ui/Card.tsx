460aa9985b405f788d077fca823a618909bac864
Ege Uysal <hello@egeuysal.com>
2025-04-26 12:36:18 -0500

    Update

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
 type CardProps = {
   icon?: React.ReactNode;
   title: string;
   desc: string;
-}
+  date?: string;
+};
 
-const Card: React.FC<CardProps> = ({ icon, title, desc}) => {
+const Card: React.FC<CardProps> = ({ icon, title, desc, date }) => {
   return (
     <article className="bordered flex flex-col gap-1 w-full rounded-lg p-4">
       <aside className="text-black dark:text-white">{icon}</aside>
-      <h6 className='w-full'>{title}</h6>
-      <p className='w-full'>{desc}</p>
+      <section>
+        <h6 className="w-full">{title}</h6>
+        <p className="w-full opacity-50">{date}</p>
+      </section>
+      <p className="w-full">{desc}</p>
     </article>
   );
-}
+};
 
 export default Card;
